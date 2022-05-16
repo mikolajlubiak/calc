66b43b80aba58467bfc20a6682b3c8c42966c74f
Mikolaj Lubiak <galanonim@disroot.org>
2022-05-16 20:24:04 +0200

    Added support for (brackets, powers, squared roots) in Python version.

--- calc.py
+++ calc.py
@@ -1,54 +1,84 @@
 #!/usr/bin/env python3
 
 from sys import argv
+from math import sqrt
+
+def parens(s: str, i: int):
+    openers = {"(": ")"}
+    closers = {v: k for k, v in openers.items()}
+    stack = []
+    result = []
+    for ii in range(i, len(s)):
+        c = s[ii]
+        if c in openers:
+            stack.append([c, ii])
+        elif c in closers:
+            if not stack:
+                raise ValueError(f"Tried to close brace without an open at position {i}.")
+            pair, idx = stack.pop()
+            if pair != closers[c]:
+                raise ValueError(f"Mismatche brace at position {i}.")
+            if idx == i:
+                return ii
+    if stack:
+        raise ValueError(f"No closing brace at position {i}.")
+    return result
 
 def calc(s: tuple):
     so = list(s)
-    if "(" in s and ")" in s:
-        for i in range(len(s)):
-            if s[i] == "(":
-                for x in range(len(s)):
-                    if s[x] == ")":
-                        sb = []
-                        for y in range(i, x+1):
-                            sb.append(so[i])
-                            so.pop(i)
-                        sb.pop(0)
-                        sb.pop()
-                        so.insert(i, calc(sb))
-                        return calc(tuple(so))
-    elif "*" in s or "/" in s:
-        for i in range(len(s)):
-            if s[i] == "*":
-                so.pop(i-1)
-                so.pop(i-1)
-                so.pop(i-1)
-                so.insert(i-1, float(s[i-1]) * float(s[i+1]))
-                return calc(tuple(so))
-            elif s[i] == "/":
-                so.pop(i-1)
-                so.pop(i-1)
-                so.pop(i-1)
-                so.insert(i-1, float(s[i-1]) / float(s[i+1]))
-                return calc(tuple(so))
-    elif "+" in s or "-" in s:
-        for i in range(len(s)):
-            if s[i] == "+":
-                so.pop(i-1)
-                so.pop(i-1)
-                so.pop(i-1)
-                so.insert(i-1, float(s[i-1]) + float(s[i+1]))
-                return calc(tuple(so))
-            elif s[i] == "-":
-                so.pop(i-1)
-                so.pop(i-1)
-                so.pop(i-1)
-                so.insert(i-1, float(s[i-1]) - float(s[i+1]))
-                return calc(tuple(so))
-    else:
-        return float(s[0])
-try:
-    print(calc(argv[1].split(" ")))
-except:
-    a = input("Give an expression: ")
-    print(calc(a.split(" ")))
+    for i in range(len(s)):
+        if s[i] == "(":
+            d = parens(s, i)
+            sb = []
+            for x in range(i, d+1):
+                sb.append(so[i])
+                so.pop(i)
+            sb.pop(0)
+            sb.pop()
+            so.insert(i, calc(sb))
+            return calc(tuple(so))
+    for i in range(len(s)-1):
+        if s[i] == "**":
+            so.pop(i-1)
+            so.pop(i-1)
+            so.pop(i-1)
+            so.insert(i-1, float(s[i-1]) ** float(s[i+1]))
+            return calc(tuple(so))
+        elif s[i] == "//":
+            so.pop(i)
+            so.pop(i)
+            so.insert(i, sqrt(float(s[i+1])))
+            return calc(tuple(so))
+    for i in range(1, len(s)-1):
+        if s[i] == "*":
+            so.pop(i-1)
+            so.pop(i-1)
+            so.pop(i-1)
+            so.insert(i-1, float(s[i-1]) * float(s[i+1]))
+            return calc(tuple(so))
+        elif s[i] == "/":
+            so.pop(i-1)
+            so.pop(i-1)
+            so.pop(i-1)
+            so.insert(i-1, float(s[i-1]) / float(s[i+1]))
+            return calc(tuple(so))
+    for i in range(1, len(s)-1):
+        if s[i] == "+":
+            so.pop(i-1)
+            so.pop(i-1)
+            so.pop(i-1)
+            so.insert(i-1, float(s[i-1]) + float(s[i+1]))
+            return calc(tuple(so))
+        elif s[i] == "-":
+            so.pop(i-1)
+            so.pop(i-1)
+            so.pop(i-1)
+            so.insert(i-1, float(s[i-1]) - float(s[i+1]))
+            return calc(tuple(so))
+    return float(s[0])
+if __name__ == "__main__":
+    try:
+        print(calc(argv[1].split(" ")))
+    except IndexError:
+        a = input("Give an expression: ")
+        print(calc(a.split(" ")))
